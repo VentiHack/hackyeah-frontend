5a629e3daa6deea43dab46af03dc4091f17b7227
Sosek1 <68021413+Sosek1@users.noreply.github.com>
2023-10-01 11:44:15 +0200

    mapa dziala eeeeeeesa(chujowo ale stabilnie)

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,72 +1,77 @@
 import { LatLngExpression } from "leaflet";
 import { useEffect, useState } from "react";
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 type Data = {
-    id: number;
-    img: string;
-    animalSpecies: string;
-    knownAnimalSpecies: boolean;
-    latitude: number;
-    longitude: number;
-    additionalInfo: string;
-    createdAt: Date;
+  id: number;
+  img: string;
+  animalSpecies: string;
+  knownAnimalSpecies: boolean;
+  latitude: number;
+  longitude: number;
+  additionalInfo: string;
+  createdAt: Date;
+};
+
+type MarkerData = {
+  x: number;
+  y: number;
+  animalSpecies: string;
 };
 
 const fetchDataHandler = async () => {
-    try {
-        return await fetch("http://10.250.162.170:3000/api")
-            .then((data) => data.json())
-            .then((data) =>
-                data.map((entry: Data) => [
-                    entry.latitude - 1.85,
-                    entry.longitude + 0.8,
-                ])
-            );
-    } catch (error) {
-        console.error(error);
-        return;
-    }
+  try {
+    return await fetch("http://10.250.162.30:3000/api")
+      .then((data) => data.json())
+      .then((data) =>
+        data.map((entry: Data) => ({
+          x: entry.latitude - 1.85,
+          y: entry.longitude + 0.8,
+          animalSpecies: entry.animalSpecies,
+        }))
+      );
+  } catch (error) {
+    console.error(error);
+    return;
+  }
 };
 
 const Map = () => {
-    const [markerData, setMarkerData] = useState<[number, number][]>();
+  const [markerData, setMarkerData] = useState<MarkerData[]>();
 
-    useEffect(() => {
-        fetchDataHandler().then((markers) => setMarkerData(markers));
-    }, []);
+  useEffect(() => {
+    fetchDataHandler().then((markers) => setMarkerData(markers));
+  }, []);
 
-    const center: LatLngExpression =
-        markerData && markerData.length > 0 ? markerData[0] : [50, 20];
+  const center: LatLngExpression =
+    markerData && markerData.length > 0
+      ? [markerData[0].x, markerData[0].y]
+      : [50, 20];
 
-    console.log(markerData);
-
-    return (
-        <>
-            <link
-                rel="stylesheet"
-                href="https://unpkg.com/leaflet/dist/leaflet.css"
-            />
-            <div className="h-[80svh]">
-                <MapContainer center={center} zoom={10} zoomControl={true}>
-                    <TileLayer
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                    {markerData &&
-                        markerData.map((marker, id) => {
-                            console.log(marker);
-                            return (
-                                <Marker 
-                                    position={[marker[0], marker[1]]}
-                                    key={id}
-                                />
-                            );
-                        })}
-                </MapContainer>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/leaflet/dist/leaflet.css"
+      />
+      <div className="h-[80svh]">
+        <MapContainer center={center} zoom={10} zoomControl={true}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {markerData &&
+            markerData.map((marker, id) => {
+              return (
+                <Marker position={[marker.x, marker.y]} key={id}>
+                  <Popup>{marker.animalSpecies}</Popup>
+                </Marker>
+              );
+            })}
+        </MapContainer>
+      </div>
+    </>
+  );
 };
 
 export default Map;
